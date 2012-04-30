c6d5d3f0cca561f17119ff240c4cea4a5c8699d2
Greg Allen <me@jga.me>
2012-04-29 20:59:14 -0700

    initial work on moncmd

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var port = process.argv[2] || 8001;
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
