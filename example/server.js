6beadd5c64d9304410acd5ad9019f36af5229c5b
Greg Allen <me@jga.me>
2012-05-14 09:59:09 -0700

    added less and tail commands

--- example/server.js
+++ example/server.js
@@ -4,4 +4,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(port, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:'+port+'/');
