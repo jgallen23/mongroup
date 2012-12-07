68202162d438133ec81d52463bcfada9e5fa9b3e
Kristoffer Berdal <web@flexd.net>
2012-12-07 16:29:06 +0100

    This fixes a lot of issues talked about in #5
    Change config-format to solve some problems.
    This commit changes how the config format works.
    We take advantage of bash's built-in file sourcing
    abilities to allow us to have a config file without
    reading a file like before. This solution is more
    robust, and it fixes several flaws with the old system
    mainly:
      * You can have commands with = in them, something that would break the
      * old system. And you we also no longer break if there is a whitespace
      * in our command :)

--- example/server.js
+++ example/server.js
@@ -3,5 +3,5 @@ var port = process.argv[2] || 8001;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(port, '127.0.0.1');
+}).listen(port);
 console.log('Server running at http://127.0.0.1:'+port+'/');
